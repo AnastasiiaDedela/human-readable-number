6d2472b701043dc2be74dbfa14f9a12b0bc5e8ec
AnastasiiaDedela <nastyadedela@gmail.com>
2022-02-22 16:33:54 +0200

    feat: completed js properties for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+    const units = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    let neededDigit = number % 10;
+    
+    if (number < 20) { 
+        return units[number];
+    } else if (number < 100) {
+        return tens[~~(number/10)-2] + (neededDigit? " " + units[neededDigit]: "");
+    } else if (number < 1000) {
+        return units[~~(number/100)] + " hundred" + (number%100 == 0? "" : " " + toReadable(number%100));
+    } else {
+        return toReadable(~~(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
+    }
 }
